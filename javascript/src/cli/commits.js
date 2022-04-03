723c3d1c2e1ff0b9d1dfcdf48fb2d35812685a58
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2022-04-03 18:30:59 -0500

    Use Request

--- javascript/src/cli/commits.js
+++ javascript/src/cli/commits.js
@@ -1,83 +1,53 @@
 const fs = require("fs");
 const {Octokit} = require("@octokit/core");
 const commander = require("commander");
+const RequestBuilder = require("../lib/RequestBuilder");
+const Formatter = require("../lib/Formatter");
 
 // Setup process
 
 commander.version("1.0.0", "-v, --version")
     .requiredOption("-t, --token <value>", "Sets the GitHub access token to use")
     .requiredOption("-r, --repo <value>", "Sets the GitHub repository (obsproject/obs-studio)")
-    .option("-o, --output <value>", "Sets the output file", "issues.csv")
+    .option("-o, --output <value>", "Sets the output file", "commits.csv")
     .parse(process.argv);
 commander.parse();
 const options = commander.opts();
 
-// Setup Octokit
-const octokit = new Octokit({auth: options.token});
+let builder = new RequestBuilder(options.token, options.repo, "commits");
+builder.option("state", "all");
+builder.exclude("pull_request");
+builder.multiPage(true);
+builder.submit().then(res => {
+    const keys = new Map();
+    keys.set("id", null);
+    keys.set("commit", formatCommit);
+    keys.set("committer", formatCommitter);
+    keys.set("author", formatAuthor);
 
-// These are all the keys and functions to format data for each key
-// null means it will simply add the string value to the csv field
+    // console.log(res.data); // Print data
+    // console.log(res.toCsv(keys)) // Print CSV
 
-const keys = new Map();
-keys.set("id", null);
-keys.set("committer", formatDate);
-keys.set("tree", formatURL);
+    res.toCsvFile(keys, options.output, err => {
+        if (err) throw err; else console.log(`Wrote CSV to ${options.output}`);
+    })
+});
 
-// Create CSV header
-let csv = "";
-for (let entry of keys.entries()) csv += `${entry[0]},`;
-csv = csv.slice(0, -1) + "\n"; // Remove the last comma and add new line
-
-async function generate_csv() {
-    let res = [];
-    let page = 1;
-
-    res.status = 200; // Dirty trick to get the loop going
-    res.data = [1];
-   // res.labels = ' ';
-
-
-    while (res.status !== 404 && res.data.length !== 0) {
-        res = await octokit.request(`GET /repos/${options.repo}/issues`, {
-            state: "all", per_page: 100, // GitHub's API max items per page is 100
-            page
-        });
-
-        page++;
-        if (res.status !== 404) csv += formatData(res.data);
-        //if (res.status !== 404) csv += formatData(res.labels);
-    }
-    return csv;
+function formatCommit(data) {
+    // TODO
 }
 
-function formatData(data) {
-    let csv = "";
-    for (let i = 0; i < data.length; i++) {
-        if (!data[i]["pull_request"]) {     // Ignore pull requests
-            for (const [key, action] of keys.entries()) {
-                csv += (data[i][key] && action) ? `${action(data[i][key])},` : `${data[i][key]},`;
-            }
-            csv = csv.slice(0, -1) + "\n";
-        }
-    }
-    return csv;
+function formatCommitter(data) {
+    // TODO
 }
 
-function formatDate(data) {
-    if (data) {
-        let date = new Date(data);
-        return `${date.getUTCMonth() + 1}/${date.getUTCDate()}/${date.getUTCFullYear()}`;
-    } else return "null"
+function formatAuthor(data) {
+    // TODO
 }
+
 function formatURL(data) {
     if (data) {
         let url = new URL(data);
         return url;
     } else return "null"
 }
-generate_csv().then(csv => {
-    console.log(`Writing to ${options.output}`);
-    fs.writeFile(options.output, csv, err => {
-        if (err) throw err; else console.log(`${options.output} written successfully.`);
-    });
-}).catch(console.error);
